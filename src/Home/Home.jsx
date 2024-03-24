100cbba93c8ae43b01e9a9340bfe6809c213876b
vitaliip <kelezar4477@gmail.com>
2024-03-24 18:57:36 +0100

    deploy test

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,28 +1,10 @@
 import styles from "./Home.module.css"
-import one from "../../public/1.png"
-import two from "../../public/2.png"
-import three from "../../public/3.png"
-import four from "../../public/4.png"
-import five from "../../public/5.png"
-import six from "../../public/6.png"
 import plant from "../assets/plant.png"
 import {Link} from "react-router-dom";
 import {HOME_ROUTE, NEW_ROUTE, POPULAR_ROUTE} from "../utils/consts.jsx";
+import {flowers_category} from "../database.js";
 
-let flowers = [
-    {name: "Магнолії", image: one, new: false, popular: true},
-    {name: "Ялина - Picea", image: two, new: true, popular: true},
-    {name: "Ялиці - Abies", image: three, new: true, popular: true},
-    {name: "Листяні кущі", image: four, new: false, popular: false},
-    {name: "Японські клени", image: five, new: false, popular: false},
-    {name: "Штамбові авойні рослини", image: six, new: false, popular: true},
-    {name: "Магнолії", image: one, new: true, popular: false},
-    {name: "Ялина - Picea", image: two, new: true, popular: true},
-    {name: "Ялиці - Abies", image: three, new: true, popular: false},
-    {name: "Листяні кущі", image: four, new: true, popular: false},
-    {name: "Японські клени", image: five, new: false, popular: false},
-    {name: "Штамбові авойні рослини", image: six, new: false, popular: false},
-]
+
 
 const Home = (filter) => {
     const active = {color: "white", backgroundColor: "#79A03F"}
@@ -49,7 +31,7 @@ const Home = (filter) => {
                 </div>
             </div>
             <div className={styles.categoryGrid}>
-                {flowers.filter(element => filter.filter=== "all" ? true : element[filter.filter])
+                {flowers_category.filter(element => filter.filter=== "all" ? true : element[filter.filter])
                     .map((element, index) => (
                         <div className={styles.categoryCard} key={index}>
                             <div className={styles.imageHolder}>
---
+++ src/database.js
@@ -0,0 +1,22 @@
+import one from "../public/1.png";
+import two from "../public/2.png";
+import three from "../public/3.png";
+import four from "../public/4.png";
+import five from "../public/5.png";
+import six from "../public/6.png";
+
+export const flowers_category = [
+    {name: "Магнолії", image: one, new: false, popular: true},
+    {name: "Ялина - Picea", image: two, new: true, popular: true},
+    {name: "Ялиці - Abies", image: three, new: true, popular: true},
+    {name: "Листяні кущі", image: four, new: false, popular: false},
+    {name: "Японські клени", image: five, new: false, popular: false},
+    {name: "Штамбові авойні рослини", image: six, new: false, popular: true},
+    {name: "Магнолії", image: one, new: true, popular: false},
+    {name: "Ялина - Picea", image: two, new: true, popular: true},
+    {name: "Ялиці - Abies", image: three, new: true, popular: false},
+    {name: "Листяні кущі", image: four, new: true, popular: false},
+    {name: "Японські клени", image: five, new: false, popular: false},
+    {name: "Штамбові авойні рослини", image: six, new: false, popular: false},
+]
+
